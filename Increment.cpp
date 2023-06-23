e62ccdc57593011c14a4a62ddc2f6e116e631fa6
Sahilminhas07 <98466660+Sahilminhas07@users.noreply.github.com>
2023-06-23 23:37:40 +0530

    Add files via upload

---
+++ Increment.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void increment(int&a){
+    a++;
+    return;
+}
+int main(){
+    int i=11;
+    int j=i;
+    j++;
+    //increment(i);
+    cout<<i<<endl;
+
+    return 0;
+}
